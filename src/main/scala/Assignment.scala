a5859e1dd5da4c159efa6ba89fe4ff80fbe3d1c7
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-09 20:25:41 +0530

    Cassandra assignment

---
+++ src/main/scala/Assignment.scala
@@ -0,0 +1,64 @@
+import com.datastax.driver.core.{Session, Cluster}
+
+/**
+  * Created by knoldus on 9/4/17.
+  */
+class Assignment {
+
+  val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+
+  val session: Session = cluster.connect("assignment")
+
+  def selectByEmail(email:String) = {
+    // session.execute("insert into video(video_name,userid,video_id,year) values('Bahubhali ',1,109,2015);")
+    val records = session.execute(s"select * from user where email='$email'")
+    val iterate = records.iterator()
+
+    while (iterate.hasNext) {
+
+      println(iterate.next())
+
+    }
+
+  }
+
+  def selectByVideoName(videoname:String) = {
+
+    val records = session.execute(s"select * from user from video = $videoname")
+    val iterate = records.iterator()
+
+    while (iterate.hasNext) {
+
+      println(iterate.next())
+
+    }
+
+  }
+
+  def selectVideoByYear(year:Int) = {
+
+    val records = session.execute(s"select * from user from videoByYear where year > $year allow filtering")
+    val iterate = records.iterator()
+
+    while (iterate.hasNext) {
+
+      println(iterate.next())
+
+    }
+
+  }
+
+  def selectVideoByIdAndYear(userId:Int) = {
+
+    val records = session.execute(s"select * from videoByYear where userId = $userId ORDER BY year DESC LIMIT 5;")
+    val iterate = records.iterator()
+
+    while (iterate.hasNext) {
+
+      println(iterate.next())
+
+    }
+
+  }
+  cluster.close()
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+
+object Main extends App{
+
+ val obj = new Assignment
+  obj.selectByEmail("mahesh.kndpl@gmail.com")
+  obj.selectByVideoName("Rustom")
+  obj.selectVideoByYear(2015)
+  obj.selectVideoByIdAndYear(2)
+
+}
